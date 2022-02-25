bc4eda662a4375d743b909425fa98e8f6058ed96
ThongPro <hoangthong19092002@gmail.com>
2022-02-25 09:32:52 +0700

    thong update code

--- controller/FormController.js
+++ controller/FormController.js
@@ -1,55 +1,57 @@
 
-  const api1 = "http://localhost:3000/sv";
-  function formController($scope, $rootScope, $http) {
-    $rootScope.index = -1;
-    $rootScope.sv = { gender: "1" };
+const api1 = "http://localhost:3000/sv";
+function formController($scope, $rootScope, $http) {
+  $rootScope.index = -1;
+  $rootScope.sv = { gender: "1" };
 
-    $scope.onFormSubmit = function (event) {
-      event.preventDefault();
-    }
-    $scope.onInsert = function () {
-      console.log($scope.sv);
-      if ($scope.sv.fullname != null &&
-        $scope.sv.name != null &&
-        $scope.sv.email != null &&
-        $scope.sv.pass != null &&
-        $scope.sv.pass1 != null &&
-        $scope.sv.address != null) {
-        if ($scope.sv.pass == $scope.sv.pass1) {
-          $scope.sv.id = null;
-          $http.post(api1, $scope.sv)
-            .then(function (response) {
-              console.log(response);
-              $scope.message = "Thêm mới thành công";
-              $rootScope.isSuccess = true;
-              alert("Thêm Mới Thành Công");
-            })
-        } else {
-          alert("Mật Khẩu không trùng với Mật Khẩu Xác Nhận");
-        }
+  $scope.onFormSubmit = function (event) {
+    event.preventDefault();
+  }
+  $scope.onInsert = function () {
+    console.log($scope.sv);
+    if ($scope.sv.fullname != null &&
+      $scope.sv.name != null &&
+      $scope.sv.email != null &&
+      $scope.sv.pass != null &&
+      $scope.sv.pass1 != null &&
+      $scope.sv.address != null) {
+      if ($scope.sv.pass == $scope.sv.pass1) {
+        $scope.sv.id = null;
+        $http.post(api1, $scope.sv)
+          .then(function (response) {
+            console.log(response);
+            $scope.message = "Thêm mới thành công";
+            $rootScope.isSuccess = true;
+            alert("Thêm Mới Thành Công");
+          })
       } else {
-        alert("Vui lòng điền đầy đủ thông tin trên Form");
+        alert("Mật Khẩu không trùng với Mật Khẩu Xác Nhận");
       }
-    }
-
-    $scope.onUpdate = function () {
-      $http.put(Update, $scope.sv)
-        .then(function (response) {
-          console.log(response.data);
-        })
-    }
-
-
-    $scope.onDelete = function () {
-      console.log(Update);
-      $http.delete(Update, $scope.sv)
-        .then(function (response) {
-          console.log(response)
-        })
-    }
-
-    $scope.onClear = function () {
-      $rootScope.index = -1;
-      $rootScope.sv = { gender: "1" };
+    } else {
+      alert("Vui lòng điền đầy đủ thông tin trên Form");
     }
   }
+
+  $scope.onUpdate = function () {
+    $http.put(Update, $scope.sv)
+      .then(function (response) {
+        console.log(response.data);
+        alert("Sửa Thành Công");
+      })
+  }
+
+
+  $scope.onDelete = function () {
+    console.log(Update);
+    $http.delete(Update, $scope.sv)
+      .then(function (response) {
+        console.log(response)
+        alert("Xóa Thành Công");
+      })
+  }
+
+  $scope.onClear = function () {
+    $rootScope.index = -1;
+    $rootScope.sv = { gender: "1" };
+  }
+}
--- controller/quizController.js
+++ controller/quizController.js
@@ -1,26 +1,17 @@
 function quizController($scope, $rootScope, $http) {
     $scope.quizs = [];
-    $scope.start = [];
+    $scope.start = 0;
     $scope.check = false;
-
-    $scope.start = function () {
-        console.log($scope.isLogin);
-        if ($scope.isLogin == false) {
-            alert("Vui lòng đăng nhập!");
-            return;
-        }
-        $scope.check = false;
-        $scope.baiLam = []
-    }
 
     const api = "http://localhost:3000/quizs";
     $http.get(api)
         .then(function (response) {
             $scope.quizs = response.data;
-            for(i=1;i<$scope.quizs;i++){
-            $scope.start ++;
-            }
             console.log($scope.quizs);
         });
+        $scope.final=function(){
+            alert("Bạn Đã Trượt");
+          window.location = "http://127.0.0.1:5501/home.html";
+        }
         
 }
